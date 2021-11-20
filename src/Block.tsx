44e92f225ba01cb5e7ab945092ed25ee71212bdd
CauaRojas <cauarojas@hotmail.com>
2021-11-20 11:22:21 -0300

    Criei um diretorio de css

--- src/Block.tsx
+++ src/Block.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 export default function Bloco(){
+    function fnAlerta() {
+        alert("teste")
+        return 0;
+    }
+
     return(
-        <><div style={{backgroundColor : "red",width: "100px",aspectRatio:"1/1"}}></div></>
+        <><div>
+            <button onClick={() => fnAlerta()}>Clica em mim ai seu viadinho</button>   
+        </div></>
     )
+
 }
