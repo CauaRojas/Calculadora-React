742e3f69e2788729aa26ab992876bf54fccfb305
CauaRojas <cauarojas@hotmail.com>
2021-11-20 11:31:44 -0300

    resetei o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './styles/App.css';
-import Bloco from './Block';
 
 function App() {
   return (
-    <>
-    <h1>Hello World!</h1>
-    <Bloco />
-    </>
+    <h1>Hello World</h1>
   );
 }
 
